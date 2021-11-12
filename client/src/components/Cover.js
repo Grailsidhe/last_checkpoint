8ad93b0136d5efc947afd8b583e5fc941a6d5fc6
Lorianne Aguilar <78496780+Grailsidhe@users.noreply.github.com>
2021-11-12 08:43:34 +0100

    Update Cover.js

--- client/src/components/Cover.js
+++ client/src/components/Cover.js
@@ -1,14 +1,13 @@
 import React from "react";
 // import { Link } from "react-router-dom";
 import "./css/Cover.css";
-import "./css/vivify.min.css";
 
 export default function Cover() {
 
     return (
         <div className="Cover-wrapper">
             <div className="Cover-intro">
-                <div className="Cover-title vivify blink delay-900 duration-1000">dev.lorianne</div>
+                <div className="Cover-title">dev.lorianne</div>
             </div>
 
         </div>
